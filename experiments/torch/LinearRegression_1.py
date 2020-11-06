e01a0b0e3297fee051b2679004cb96857c8329b4
Partnernet Software <business@partnernetsoftware.com>
2020-11-06 15:42:08 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   LinearRegression_1.py

--- experiments/torch/LinearRegression_1.py
+++ experiments/torch/LinearRegression_1.py
@@ -6,15 +6,20 @@ class main(mytorch):
     def __init__(self):
         # print("LinearRegression.__init__")
         super().__init__()
-        # self.linear = torch.nn.Linear(in_features=10, out_features=10) # in/out dimension (1=>1)
+        w = 1
+        # self.w = w
+        self.linear = torch.nn.Linear(in_features=w, out_features=w) # in/out dimension
+        # self.model = torch.nn.Linear(in_features=w, out_features=w) # in/out dimension
 
     def forward(self, x):
-        w = list(x.size())[0] # TODO...
-        print('w=', w)
-        # linear = self.linear
-        # w = 10
-        linear = torch.nn.Linear(w,w)
-        return linear(x) # y = a*x + b 
+        return self.linear(x)
+
+        # w = list(x.size())[0] # TODO...
+        # print('TODO w=', w)
+        # #  linear = self.linear
+        # # w = 10
+        # linear = torch.nn.Linear(w,w)
+        # return linear(x) # y = a*x + b 
 
 if __name__ == '__main__':
 
@@ -24,39 +29,56 @@ if __name__ == '__main__':
     mod = main()
     if _cuda: mod = mod.cuda()
 
+    # mod_parameters = mod.parameters()
+    # print("mod_parameters=",mod_parameters)
+    #mod_parameters = mod.parameters()
+    #print("mod_parameters=",list(mod_parameters))
+
     # if _cuda: x = x.cuda()
 
     from torch.autograd import Variable
-    x = torch.rand(10)
+    from matplotlib import pyplot as plt
+
+    x = torch.rand(10,1)
+    y = torch.rand(10,1)
     print('x=',x)
-    xx = Variable(x, requires_grad=True)
-    # xx = Variable(x)
+    print('y=',y)
+    plt.plot(x.numpy(), y.numpy(), 'ro', label='org data')
+
+    # xx = Variable(x, requires_grad=True)
+    xx = Variable(x)
     print('xx=',xx)
 
-    y = torch.rand(10)
-    print('y=',y)
+    for i in range(1000):
+    #if True:
+        out_y = mod(xx)
+        print('out_y=', out_y)
+        criterion = torch.nn.MSELoss()
+        loss = criterion(out_y, y)
+        #print('1.lost.data=',loss.data)
 
-    out_y = mod(xx)
-    print('out_y=', out_y)
-    criterion = torch.nn.MSELoss()
-    loss = criterion(out_y, y)
-    print('lost.data=',loss.data)
+        # backward
+        mod_parameters = mod.parameters()
+        # print("mod_parameters=",mod_parameters)
+        optimizer = torch.optim.SGD(mod_parameters, lr=1e-3)
+        #optimizer = torch.optim.SGD(mod.parameters(), lr=1e-3)
+        optimizer.zero_grad() # reset grad.
+        loss.backward()
+        optimizer.step()
+        print('lost.data=',loss.data)
+        #the_loss = loss.data[0]
+        #print(the_loss)
 
-    # backward
-    mod_parameters = mod.parameters()
-    print("mod_parameters=",mod_parameters)
-    optimizer = torch.optim.SGD(mod_parameters, lr=1e-3)
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print('lost.data=',loss.data)
+    mod.eval()
 
     # rst = mod.forward(x)
     # print(x, rst)
 
-    # from matplotlib import pyplot as plt
     # if _cuda:
     #     plt.plot(x.cpu().numpy(),rst.cpu().detach().numpy(),'ro',label='test')
     # else:
-    #     plt.plot(x.numpy(),rst.detach().numpy(),'ro',label='test')
-    # plt.show()
+    if True:
+        # x_train = Variable(torch.rand(10))
+        predict = mod(xx).data.numpy()
+        plt.plot(xx.detach().numpy(), predict, label='line')
+    plt.show()
